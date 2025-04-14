0ce0497a88a1ced74b38c3aab9e810af6cf19406
Shivesh Upadhyay <162001314+Shivesh0305@users.noreply.github.com>
2025-04-14 08:34:01 +0530

    Delete Chapter 5 directory

--- Chapter 5/Practice Questions/Chapter 5 Practice Q2.py
+++
@@ -1,9 +0,0 @@
-a=input("enter a number")
-b=input("enter a number")
-c=input("enter a number")
-d=input("enter a number")
-e=input("enter a number")
-f=input("enter a number")
-g={int(a),int(b),int(c),int(d),int(e),int(f)}
-print(g)
-print(type(g))
--- Chapter 5/Practice Questions/Chapter 5 Practice Q4(imp).py
+++
@@ -1,6 +0,0 @@
-s=set()
-s.add(20)
-s.add(20.0) #---> 20.0 and 20 is same value so only one is printed
-s.add("20")
-print(s)
-print(len(s))
