51f0cb5c89b7f5dfdedfd9cb230e0037deb11dd3
Shivesh Upadhyay <162001314+Shivesh0305@users.noreply.github.com>
2025-04-14 08:32:57 +0530

    Delete Chapter 10 directory

--- Chapter 10/Chapter 10(c).py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    company="Google"
-    def getSalary(self):
-        print("Salary is "+ self.salary)
-
-harry=Employee()
-harry.salary="10 Cr"
-harry.getSalary()
-#harrygetSalary()=Employee.getSalary(harry)
